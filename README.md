bee2818e8d38401cf46918995262938c521fc2c0
Whirlwindraven <fadysherif1998@gmail.com>
2023-09-14 14:10:43 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # infinite_
+
+## Table of Contents
+- [Project Description](#Description)
+- [Screenshot](#Screenshot)
+- [Installation](#Installation)
+- [Contributing](#Contributing)
+- [Questions](#Questions)
+- [License](#License)
+
+## Description
+An infinate scroll of pictures at your disposal 
+
+## Screenshot-of-app
+Sample of the application is found in the screenshot below 
+
+## Installation
+Through github.
+
+## Contributing
+Please reach out through GitHub.
+
+## Questions
+fadysherif1998@gmail.com
+
+## License
+Licensed under the [MIT](https://choosealicense.com/licenses/mit/) license.
--- script.js
+++ script.js
@@ -1,15 +1,76 @@
-const count = 10;
+const imageContainer = document.querySelector('.image-container');
+const loader = document.querySelector('.loader');
+let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+const count = 30;
 const apiKey = 'Pu4PY9RSK0YoWHYzmSZSk0nvd_xbTJ_nlzT1fL8oIVs';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+function imageLoaded() {
+imagesLoaded++;
+if(imagesLoaded === totalImages){
+    ready = true;
+    loader.hidden = true;
+}
+}
+
+
+function setAttributes(element, attributes){
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key] );
+    }
+}
+
+
+function displayPhotos (){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+    
+        setAttributes(item, {
+            'href': photo.links.html,
+            'target': '_blank',
+        });
+
+        const img = document.createElement('img');
+
+        setAttributes(img, {
+            'src': photo.urls.regular,
+            'alt': photo.alt_description,
+            'title': photo.alt_description,
+        })
+
+        img.addEventListener('load', imageLoaded)
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+    })
+}
+
 
 async function getPhotos(){
     try{
 const response = await fetch(apiUrl);
-const data = await response.json
-console.log(data);
-    } catch (error){
+ photosArray = await response.json();
+displayPhotos();
+
+    } catch (error) {
+        console.error("Error fetching photos:", error);
 
     }
 }
 
-getPhotos
+window.addEventListener('scroll', () => {
+
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+       ready = false;
+        getPhotos();
+    }
+})
+
+getPhotos();
